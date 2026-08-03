c53f42501293aefbd4c82a0b5ca07703e0638486
sybertoooth7-lgtm <sybertoooth7@gmail.com>
2026-08-03 11:56:31 +0300

    Change import statement for Resend module

--- backend/src/lib/email.js
+++ backend/src/lib/email.js
@@ -1,4 +1,4 @@
-import Resend from 'resend';
+import { Resend } from 'resend';
 
 const resend = process.env.RESEND_API_KEY ? new Resend(process.env.RESEND_API_KEY) : null;
 const FROM_EMAIL = process.env.FROM_EMAIL || '';
