5781ef7cf388f74007945b08da2d31c80fd73466
sybertoooth7-lgtm <sybertoooth7@gmail.com>
2026-08-13 08:51:29 +0300

    Add new routes for verification and policies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,11 @@ import { Routes, Route } from 'react-router';
 import HomePage from './pages/HomePage';
 import ClientLogin from './pages/ClientLogin';
 import ClientDashboard from './pages/ClientDashboard';
+import VerifyScore from './pages/VerifyScore';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
+import SecurityPolicy from './pages/SecurityPolicy';
+import CompliancePage from './pages/CompliancePage';
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/client/login" element={<ClientLogin />} />
       <Route path="/client/dashboard" element={<ClientDashboard />} />
+      <Route path="/verify/:token" element={<VerifyScore />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms-of-service" element={<TermsOfService />} />
+      <Route path="/security" element={<SecurityPolicy />} />
+      <Route path="/compliance" element={<CompliancePage />} />
     </Routes>
   );
 }
