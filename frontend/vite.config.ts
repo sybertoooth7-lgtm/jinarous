3b22ab04b828d7be17b29056ba74a656e70c844b
sybertoooth7-lgtm <sybertoooth7@gmail.com>
2026-08-25 12:18:32 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,16 @@
-// frontend/vite.config.ts
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-  ],
+  base: './',
+  plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:4000',
         changeOrigin: true,
       },
     },
